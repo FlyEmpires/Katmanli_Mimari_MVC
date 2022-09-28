e55191c983aaff59cea59ab8b6211cc323dfe951
Talha Acar <talha.car@hotmail.com>
2022-09-29 02:44:59 +0300

    Yazarlar Listelendi ve Yazar ekleme işlemleri yapıldı. Code First ile Yazar tablosuna yeni sütun eklendi.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı Boş Olamaz");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadı Boş Olamaz");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayın");
+            RuleFor(x => x.WriterAbout).MaximumLength(50).WithMessage("Hakkımda Kısmını Boş Bırakmayınız.");
+            //RuleFor(x => x.WriterAbout.Contains("a")).ToString().WithMessage("Hakkımda Kısmını Boş Bırakmayınız.");
+
+        }
+    }
+}
---
+++ MvcProje/Controllers/WriterController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        // GET: Writer
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult result = writerValidator.Validate(p);
+            if (result.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+            }
+            return View();
+        }
+    }
+}
